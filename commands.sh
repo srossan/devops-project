c0183782d4b13f10f758a93ef8be1ef5571249f3
Steph Rossan <srossan@gmail.com>
2020-10-03 15:04:31 -0700

    removing git clone, git repo is required to setup the python virtual machine

--- commands.sh
+++ commands.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Setup (will work out of the root's user home directory)
-cd ~
-
-# Clone github repository for this project
-git clone git@github.com:srossan/devops-project.git
-
 # Create python virtual machine
 python3 -m venv ~/.udacity-devops
 source ~/.udacity-devops/bin/activate
